1d00bb74026c2a45973fd4d5be259cf913867fd4
Menocas17 <rodolfomenocal7@gmail.com>
2024-10-05 00:59:44 -0600

    Finishing the home page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -18,9 +18,11 @@ hamButton.addEventListener('click', () => {
     header.classList.toggle('gap')
 })
 
-// fetching the members data from the Json file
+//Directory page scripts 
 
-const url = './data/members.json'
+
+
+const url = './data/members.json' // fetching the members data from the Json file
 
 const cards = document.querySelector('#cards-section');
 
@@ -41,9 +43,8 @@ const getMembersData = async () => {
 }
  
 
-// Creating the function to display the members data in the webpage 
 
-const displayMembersData = (membersData) => {
+const displayMembersData = (membersData) => {   // Creating the function to display the members data in the directory page
     membersData.forEach (member => {
         let card = document.createElement('article');
         let companyName = document.createElement('h3');
@@ -85,43 +86,221 @@ const membersDirectory = async (data) => {
     displayMembersData(members);
 }
 
-membersDirectory()
+// Home Page scripts 
 
-const gridButton = document.querySelector('#gridButton');
-const listButton = document.querySelector('#listButton');
+const weatherIcon = document.querySelector('#weather-icon');
+const weatherInfo = document.querySelector('#weather-info');
+const forecastInfo = document.querySelector('#forecast-weather-info');
+const advertising = document.querySelector('#advertising');
 
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=12.113560033331286&lon=-86.2354811759951&appid=e4ecd0c975a7d843f411d8205c8a8aa3&units=metric';
 
-const swapingViews = (button) => {
-    
-    if (button === 'list'){
-      gridButton.classList.remove('grid-button'); 
-      listButton.classList.add('list-button')
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=12.113560033331286&lon=-86.2354811759951&cnt=24&&appid=e4ecd0c975a7d843f411d8205c8a8aa3&units=metric`
 
-      cards.classList.remove('grid-view');
-      cards.classList.add('list-view');
+const fecthingCurrentWeatherData = async () => {
+    try {
+        const weatherResponse = await fetch(weatherUrl);
+        if (!weatherResponse.ok) {
+            throw new Error (`Response status: ${weatherResponse.status}`)
+        }
 
-      
-      
-
+        const weatherData = await weatherResponse.json();
+        console.log(weatherData)
+        return weatherData
     }
 
-    if (button=== 'grid') {
-        gridButton.classList.add('grid-button');
-        listButton.classList.remove('list-button');
-
-        cards.classList.add('grid-view');
-        cards.classList.remove('list-view');
+    catch (error) {
+        console.log(error.message);
     }
-    
-
-    
 }
 
-gridButton.addEventListener('click', () =>{
-    swapingViews('grid');
+const fetchingForecastWeather = async () => {
+    try {
+        const forecastResponse = await fetch(forecastUrl);
+        if (!forecastResponse.ok) {
+            throw new Error (`Response status: ${forecastResponse.status}`)
+        }
+
+        const forecastData = await forecastResponse.json();
+        return forecastData
+    }
+
+    catch (error) {
+        console.log(error.message);
+    }
+}
+
+const averageForecastTemp = async () => {
+    const forecastData = await fetchingForecastWeather();
+
+    const firstDayForecastData = forecastData.list.slice(0, 8);
+    let firstDay = 0;
+    const firstDayAverageTemp = firstDayForecastData.reduce((total, day) => total + day.main.temp, 0) / firstDayForecastData.length;
+
+    const secondDayForecastData = forecastData.list.slice(8,16)
+    let secondDay = 0;
+    const secondDayAverageTemp = secondDayForecastData.reduce((total, day) => total + day.main.temp, 0) / secondDayForecastData.length;
+
+    const thirdDayForecastData = forecastData.list.slice(16,24);
+    let thirdDay = 0;
+    const thirdDayAverageTemp = thirdDayForecastData.reduce((total, day) => total + day.main.temp, 0) / thirdDayForecastData.length;
+    
+    const firstDayData = {
+        "temp": firstDayAverageTemp,
+        "date": firstDayForecastData[0].dt_txt
+    };
+
+    const secondDayData = {
+        "temp": secondDayAverageTemp,
+        "date": secondDayForecastData[0].dt_txt
+    };
+    
+    const thirdDayData = {
+        "temp": thirdDayAverageTemp,
+        "date": thirdDayForecastData[0].dt_txt
+    };
+
+    return {
+        firstDayData,
+        secondDayData,
+        thirdDayData
+    };
+}
+
+
+async function populatingForecastWeather() {
+    const { firstDayData, secondDayData, thirdDayData } = await averageForecastTemp();
+
+    forecastInfo.innerHTML = `
+        <li>Tomorrow: <strong>${firstDayData.temp.toFixed(0)}&deg;C</strong></li>
+        <li>${new Date(secondDayData.date).toLocaleString('en-US', { weekday: 'long' })}: <strong>${secondDayData.temp.toFixed(0)}&deg;C</strong></li>
+        <li>${new Date(thirdDayData.date).toLocaleString('en-US', { weekday: 'long' })}: <strong>${thirdDayData.temp.toFixed(0)}&deg;C</strong></li>
+    `;
+}
+
+
+function populatingCurrentWeather (data){
+    weatherIcon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`);
+    weatherIcon.setAttribute('alt', 'The curretn weather icon');
+    weatherIcon.setAttribute('width', 150);
+
+    const sunriseTimestamp = data.sys.sunrise; 
+    const sunsetTimestamp = data.sys.sunset;
+
+    const sunriseDate = new Date(sunriseTimestamp * 1000);
+    const sunsetDate = new Date(sunsetTimestamp * 1000);
+
+    const sunriseTime = sunriseDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const sunsetTime = sunsetDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+    weatherInfo.innerHTML = `
+        <p><strong>${data.main.temp}&deg;</strong> C</p>
+        <p>${data.weather[0].description}</p>
+        <p>High: ${data.main.temp_max}&deg;</p>
+        <p>Low: ${data.main.temp_min}&deg;</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Sunrise: ${sunriseTime}</p>
+        <p>Sunset: ${sunsetTime}</p>
+    `
+}
+
+const displayCurrentWeather = async () => {
+    const weatherData = await fecthingCurrentWeatherData();
+
+    populatingCurrentWeather(weatherData);
+}
+
+function shuffleCompanies(companies) {
+    return companies.sort(() => Math.random() - 0.5);
+}
+
+const randomAdvertising = async () => {
+
+    const membersData = await getMembersData ();
+    const filteredCompanies = await membersData.filter(company => company.membershipLvl >= 2)
+    const randomCompanies = shuffleCompanies(filteredCompanies);
+    const selectedCompanies = randomCompanies.slice(0, 3);
+
+    selectedCompanies.forEach(company => {
+        advertising.innerHTML += `
+        <article class="home-card ads">
+        <div class="advertising-card-title">
+            <h3>${company.name}</h3>
+            <p>${company.tagline}</p>
+        </div>
+        
+        <div class="advertising-info">
+            <img src="${company.image}" alt="The icon of ${company.name}" width="110" height="110">
+            <div>
+                <p>Email: ${company.email}</p>
+                <p>Phone: ${company.phoneNumber}</p>
+                <a href="${company.websiteUrl}">${company.websiteUrl}</a>
+            </div>
+        </div>
+
+    </article>
+        `
+    })
+    
+   
+}
+
+randomAdvertising();
+
+//Adding the addEventListener and conditinals to manage the scripts acroos the diferent pages
+
+document.addEventListener('DOMContentLoaded', ()=> {  
+    if(document.body.id === 'directory-page') {
+        membersDirectory()
+
+        const gridButton = document.querySelector('#gridButton');
+        const listButton = document.querySelector('#listButton');
+
+
+        const swapingViews = (button) => {
+            
+            if (button === 'list'){
+            gridButton.classList.remove('grid-button'); 
+            listButton.classList.add('list-button')
+
+            cards.classList.remove('grid-view');
+            cards.classList.add('list-view');
+
+            
+            
+
+            }
+
+            if (button=== 'grid') {
+                gridButton.classList.add('grid-button');
+                listButton.classList.remove('list-button');
+
+                cards.classList.add('grid-view');
+                cards.classList.remove('list-view');
+            }
+            
+
+            
+        }
+
+        gridButton.addEventListener('click', () =>{
+            swapingViews('grid');
+        })
+
+        listButton.addEventListener('click', ()=> {
+            swapingViews('list');
+        })
+    }
+
+    if(document.body.id === 'home-page') {
+       
+        
+        displayCurrentWeather();
+        populatingForecastWeather();
+    }
+
+    
 })
 
-listButton.addEventListener('click', ()=> {
-    swapingViews('list');
-})
+
 
